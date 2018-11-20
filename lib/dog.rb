9961929a14fea423177dca14e4f0cc3be55313bb
Vincent Chan <vchan313@gmail.com>
2018-11-20 15:22:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,21 @@
+<<<<<<< HEAD
 class Dog
+=======
+Class Dog
+>>>>>>> 2e40bba8bdfcf8759214d92d1c3a515e109d0b31
 
 end
 
 fido = Dog.new
 
+<<<<<<< HEAD
 snoopy = Dog.new
 
 lassie = Dog.new
+=======
+snoop = Dog.new
+
+lassie = Dog.new
+
+
+>>>>>>> 2e40bba8bdfcf8759214d92d1c3a515e109d0b31
--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class Person
+=======
+Class Person
+>>>>>>> 2e40bba8bdfcf8759214d92d1c3a515e109d0b31
 end
 
 adele_goldberg = Person.new
